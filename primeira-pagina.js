ebf5e859e9d4fc32b37b7fad4e3c3698a60f3f1a
AlisonPyter <105603679+AlisonPyter@users.noreply.github.com>
2022-08-04 18:39:09 -0300

    atualizado

---
+++ primeira-pagina.js
@@ -0,0 +1,109 @@
+
+function coracao(){
+
+    const imagem = document.getElementById('coracao');
+       
+        if(imagem.src.match("./imagens/coracao-branco.png")){
+            imagem.src = "./imagens/coracao-cheio1.png";
+        
+        }else{
+            imagem.src = "./imagens/coracao-branco.png"
+        }
+}
+
+
+function coracao1(){
+
+    const imagem = document.getElementById('coracao1');
+       
+        if(imagem.src.match("./imagens/coracao-branco.png")){
+            imagem.src = "./imagens/coracao-cheio1.png";
+        
+        }else{
+            imagem.src = "./imagens/coracao-branco.png"
+        }
+}
+
+function coracao2(){
+
+    const imagem = document.getElementById('coracao2');
+       
+        if(imagem.src.match("./imagens/coracao-branco.png")){
+            imagem.src = "./imagens/coracao-cheio1.png";
+        
+        }else{
+            imagem.src = "./imagens/coracao-branco.png"
+        }
+}
+
+function coracao3(){
+
+    const imagem = document.getElementById('coracao3');
+       
+        if(imagem.src.match("./imagens/coracao-branco.png")){
+            imagem.src = "./imagens/coracao-cheio1.png";
+        
+        }else{
+            imagem.src = "./imagens/coracao-branco.png"
+        }
+}
+
+function coracao4(){
+
+    const imagem = document.getElementById('coracao4');
+       
+        if(imagem.src.match("./imagens/coracao-branco.png")){
+            imagem.src = "./imagens/coracao-cheio1.png";
+        
+        }else{
+            imagem.src = "./imagens/coracao-branco.png"
+        }
+}
+
+function coracao5(){
+
+    const imagem = document.getElementById('coracao5');
+       
+        if(imagem.src.match("./imagens/coracao-branco.png")){
+            imagem.src = "./imagens/coracao-cheio1.png";
+        
+        }else{
+            imagem.src = "./imagens/coracao-branco.png"
+        }
+}
+
+function coracao6(){
+
+    const imagem = document.getElementById('coracao6');
+       
+        if(imagem.src.match("./imagens/coracao-branco.png")){
+            imagem.src = "./imagens/coracao-cheio1.png";
+        
+        }else{
+            imagem.src = "./imagens/coracao-branco.png"
+        }
+}
+
+function coracao7(){
+
+    const imagem = document.getElementById('coracao7');
+       
+        if(imagem.src.match("./imagens/coracao-branco.png")){
+            imagem.src = "./imagens/coracao-cheio1.png";
+        
+        }else{
+            imagem.src = "./imagens/coracao-branco.png"
+        }
+}
+
+function coracao8(){
+
+    const imagem = document.getElementById('coracao8');
+       
+        if(imagem.src.match("./imagens/coracao-branco.png")){
+            imagem.src = "./imagens/coracao-cheio1.png";
+        
+        }else{
+            imagem.src = "./imagens/coracao-branco.png"
+        }
+}
